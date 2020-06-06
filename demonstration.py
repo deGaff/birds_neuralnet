0bf2eab946a49ac12abe966aa71e14f6b9d16b27
deGaff <64227772+deGaff@users.noreply.github.com>
2020-06-06 19:18:26 +0300

    Add files via upload

--- demonstration.py
+++ demonstration.py
@@ -24,31 +24,32 @@ types = {
     2: np.int16,
     4: np.int32
 }
-
-namefile = r"Demonstration\(6).wav"
-w = wave.open(namefile, 'r')
-(nchannels, sampwidth, framerate, nframes, comptype, compname) = w.getparams()
-frames = w.readframes(nframes)
-samples = np.frombuffer(frames, dtype=types[sampwidth])
-fft_len = 1024
-thinfactor = 1
-thinned_samples = thinsamples(samples, thinfactor)
-fft = np.zeros(fft_len)
-win = signal.gaussian(fft_len, std=15)
-for i in range(len(thinned_samples) // fft_len):
-    temp = thinned_samples[i * fft_len:(i + 1) * fft_len]
-    fft += np.absolute(np.fft.fft(temp * win))
-fft = fft / (len(thinned_samples) // fft_len)
-fft = fft / fft.max()
-a = indOfFreq(100, 1024, framerate)
-b = indOfFreq(6000, 1024, framerate)
-fft = fft[a:b]
-
 RFC_model = joblib.load("neuron.pkl")
 
-RFC_predictions = RFC_model.predict(fft.reshape(1, -1))
+for i in range(1,7):
 
-print(RFC_predictions)
+    namefile = f"Demonstration\({i}).wav"
+    w = wave.open(namefile, 'r')
+    (nchannels, sampwidth, framerate, nframes, comptype, compname) = w.getparams()
+    frames = w.readframes(nframes)
+    samples = np.frombuffer(frames, dtype=types[sampwidth])
+    fft_len = 1024
+    thinfactor = 1
+    thinned_samples = thinsamples(samples, thinfactor)
+    fft = np.zeros(fft_len)
+    win = signal.gaussian(fft_len, std=15)
+    for i in range(len(thinned_samples) // fft_len):
+        temp = thinned_samples[i * fft_len:(i + 1) * fft_len]
+        fft += np.absolute(np.fft.fft(temp * win))
+    fft = fft / (len(thinned_samples) // fft_len)
+    fft = fft / fft.max()
+    a = indOfFreq(100, 1024, framerate)
+    b = indOfFreq(6000, 1024, framerate)
+    fft = fft[a:b]
+
+    RFC_predictions = RFC_model.predict(fft.reshape(1, -1))
+
+    print(RFC_predictions)
 
 
 # 1 - Nightingale
@@ -57,4 +58,3 @@ print(RFC_predictions)
 # 4 - Duck
 # 5 - Nightingale
 # 6 - Tit
-
